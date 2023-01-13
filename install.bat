3dfb04af58bd348c1f3002ca0a24cd254c7d6569
Vesper <95518085+vesperlol@users.noreply.github.com>
2023-01-13 13:30:59 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,6 @@ pip install psutil
 pip install pypiwin32
 pip install pycryptodome
 pip install cryptography
-pip install WhatObf==69
 pip install pillow
 pip install discord_webhook
 pip install pyperclip
